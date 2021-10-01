80bf11b0cd7489f877b98339eb5a18dd2d0b70be
Paweł Musioł <p.musiol1999@gmail.com>
2021-10-01 16:06:47 +0200

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,17 @@
+const path = require('path')
+
 module.exports = {
     siteMetadata: {
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: "images",
+                path: path.join(__dirname, `src`, `images`),
+            }
+        },
         `gatsby-plugin-sass`,
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`
